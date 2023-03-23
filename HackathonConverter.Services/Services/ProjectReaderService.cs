f690133d844fce8adfad6cd090c8677aa2450d06
Tamara Smith <tamara.smith@tal.com.au>
2023-03-23 11:21:58 +1100

    created initial basic reader service

---
+++ HackathonConverter.Services/Services/ProjectReaderService.cs
@@ -0,0 +1,64 @@
+using HackathonConverter.Services.Interfaces;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace HackathonConverter.Services.Services;
+
+public class ProjectReaderService
+{
+    private readonly IArguments _arguments;
+    private readonly string _FilePath;
+
+    public List<string> Projects { get; set; } = new List<string>();
+
+    public ProjectReaderService(IArguments arguments)
+    {
+        _arguments = arguments;
+        _FilePath = arguments.Path.ToString();
+    }
+
+    public List<string> GetFiles()
+    {
+        //ToDo: break this out into more methods for easier reading
+        string vbFileName = "";
+        List<string> itemList = new List<string>();
+
+        try
+        {
+            // Get information about the source directory
+            var dir = new DirectoryInfo(_FilePath);
+
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+
+            //get VBProject name
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                if (file.Extension == ".vbproj")
+                {
+                    //done an equals for now. Needs to append this probably
+                    vbFileName = file.FullName.ToString();
+                    break;
+                }
+            }
+
+            //get all vb file names we need
+            XNamespace msbuild = "http://schemas.microsoft.com/developer/msbuild/2003";
+            XDocument vbprojXml = XDocument.Load(vbFileName);
+
+            itemList = vbprojXml.Descendants(msbuild + "ItemGroup")
+                .Elements(msbuild + "Compile")
+                .Select(item => _FilePath + "\\" + item.Attribute("Include").Value)
+                .ToList();
+            
+            return itemList;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+            return itemList;
+        }
+    }
+}
--- HackathonConverter/Worker.cs
+++ HackathonConverter/Worker.cs
@@ -1,21 +1,36 @@
 using HackathonConverter.Services.Interfaces;
+using HackathonConverter.Services.Services;
+using Microsoft.Extensions.Logging;
 
 namespace HackathonConverter
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IArguments _arguments;
 
         public Worker(ILogger<Worker> logger, IArguments arguments)
         {
             _logger = logger;
-            _logger.LogDebug($"Path is the ------------ {arguments.Path} ---------------");
+            _arguments = arguments;
+            logger.LogDebug($"Path is the ------------ {arguments.Path} ---------------");
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            ProjectReaderService reader = new ProjectReaderService(_arguments);
+            List<string> files = reader.GetFiles();
+
+            foreach (string file in files)
+            {
+                _logger.LogInformation("Filename: {filename}", file.ToString());
+                //todo: vikram tasks here?
+            }
+
+            //todo: update while to be until all vikram tasks are done
             while (!stoppingToken.IsCancellationRequested)
             {
+               
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 
                 await Task.Delay(6000000, stoppingToken);
